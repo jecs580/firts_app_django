4da2aabd9e2ab23ec0b69fb71c15c259798ccb80
yorch <sanchezenrique580@gmail.com>
2019-11-15 23:20:25 -0400

    Configurando la vista desde el admin para profile y ademas anaidimos que se puediera crear un nuevo profie junto con el user

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 #Django
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Models
 from users.models import Profile
 # Register your models here.
@@ -23,3 +24,46 @@ class ProfileAdmin(admin.ModelAdmin):
     #Colocar filtros en las busquedas.
     # Si colocas created, modified django lo sobre-entiende que es un campo de fecha y mostrar muchas opciones de filtrado por fechas
     list_filter=('user__is_active','user__is_staff','created','modified')
+
+    #Variable para que customiza el detalle de un elemento de tu apliacacion
+    fieldsets=(
+        (
+            'Profile',{
+                'fields':(('user','picture'),
+                         )
+            }
+        ),
+        (
+            'Informacion Extra',{
+                'fields':(('website','phone_number'),
+                          ('biography'),
+                         )
+            }
+        ),
+        (
+            'Metadata',{
+               'fields':(('created','modified'),
+                ),
+            }
+        )
+    )
+    # Como se lee en la variable, indica que campos seran de solo lectura
+    readonly_fields =('created','modified',)
+
+    #Configurando el profile para q aparesca su creacion al crear un user
+class ProfileInline(admin.StackedInline):
+    # modelo que queremos añadir
+    model=Profile
+    # no puede ser borrado
+    can_delete=False
+    # Nombre en plural cuando se de la categoria de nuestro modelo
+    verbose_name_plural='Profilesss'
+
+#Clase q sobre-escribe la clase que ya viene por defecto para el modelo
+class UserAdmin(BaseUserAdmin):
+    # Agregar el administrador de profile al administrador de usuario base
+    inlines=(ProfileInline,)
+    list_display=('pk','username','email','first_name','last_name','is_active','is_staff')
+# Se des-registra del modelo de User para poder aplicar el mismo modelo pero con modificado enviandole el parametro del UserAdmin 
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
